e0d9d3bcfa2b110b0a14a5b9e0b3a5e6a7b5cbe4
mykolagolubyev <golubev.nikolay@gmail.com>
2022-05-26 11:22:06 -0400

    webtau 1.48

--- webtau.rb
+++ webtau.rb
@@ -1,13 +1,11 @@
 class Webtau < Formula
   desc "webtau - web test automation. command line tool to test across REST-API/Graph QL/Browser/Database/CLI"
   homepage "https://github.com/testingisdocumenting/webtau"
-  url "https://repo.maven.apache.org/maven2/org/testingisdocumenting/webtau/webtau-dist/1.45.1/webtau-dist-1.45.1-webtau.zip"
-  sha256 "f34fa06013bd9a8b65c0425cc861902f749a2320d6ece352e71ad5ebd32654e8"
-  version "1.45.1"
+  url "https://repo.maven.apache.org/maven2/org/testingisdocumenting/webtau/webtau-dist/1.48/webtau-dist-1.48-webtau.zip"
+  sha256 "6270f36f9fd9c3b78340bcb556475acec12d1154d8c04ba47f31cea5fdc2b656"
+  version "1.48"
 
   depends_on "java"
-
-  bottle :unneeded
 
   def install
     inreplace ["webtau"] do |s|
