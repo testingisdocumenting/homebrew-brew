8b3185fcefa78282ee746cd70ff3cf5a17e56e2b
mykolagolubyev <golubev.nikolay@gmail.com>
2023-02-26 16:27:38 -0500

    webtau 1.51

--- webtau.rb
+++ webtau.rb
@@ -1,9 +1,9 @@
 class Webtau < Formula
   desc "webtau - web test automation. command line tool to test across REST-API/Graph QL/Browser/Database/CLI"
   homepage "https://github.com/testingisdocumenting/webtau"
-  url "https://repo.maven.apache.org/maven2/org/testingisdocumenting/webtau/webtau-dist/1.49/webtau-dist-1.49-webtau.zip"
-  sha256 "514e106583ba5430242e59b6fc182b41309f8cbe2fb7e59bda69bd99097fcbf9"
-  version "1.49"
+  url "https://repo.maven.apache.org/maven2/org/testingisdocumenting/webtau/webtau-dist/1.51/webtau-dist-1.51-webtau.zip"
+  sha256 "37140a6c87d5b4faba3879221603a20aa562e74b0f111a056556864e93565316"
+  version "1.51"
 
   depends_on "java"
 
