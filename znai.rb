da05218b480e3e7748d642ca5aeae3892163d76d
Mykola Golubyev <golubev.nikolay@gmail.com>
2024-06-11 06:54:53 -0400

    znai 1.70 (#17)

--- znai.rb
+++ znai.rb
@@ -1,9 +1,9 @@
 class Znai < Formula
   desc "Build functional, maintainable, beautiful User Guides with Znai. Instant pages navigation. Local search. Multiple integrations to work with Python, Java, OpenAPI, etc. Transform \"getting started\" sections into slideshow for your workshops. Enterprise Docs Hub."
   homepage "https://github.com/testingisdocumenting/znai"
-  url "https://repo.maven.apache.org/maven2/org/testingisdocumenting/znai/znai-dist/1.69/znai-dist-1.69-znai.zip"
-  sha256 "0f32c113b82c4496fc91c418a961ac7da285b02d61f8b868161f328cc7312fcf"
-  version "1.69"
+  url "https://repo.maven.apache.org/maven2/org/testingisdocumenting/znai/znai-dist/1.70/znai-dist-1.70-znai.zip"
+  sha256 "54a29cfdff7a89a7345c962d2887d8d324078f489f82e839399fd8d88d691831"
+  version "1.70"
 
   depends_on "java"
 
