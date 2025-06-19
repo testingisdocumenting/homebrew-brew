25e0adcd46e3aad0449db4176b2ccb54db3020eb
MykolaGolubyev <golubev.nikolay@gmail.com>
2025-06-19 12:09:40 -0400

    fix znai 1.76 sha

--- znai.rb
+++ znai.rb
@@ -2,7 +2,7 @@ class Znai < Formula
   desc "Build functional, maintainable, beautiful User Guides with Znai. Instant pages navigation. Local search. Multiple integrations to work with Python, Java, OpenAPI, etc. Transform \"getting started\" sections into slideshow for your workshops. Enterprise Docs Hub."
   homepage "https://github.com/testingisdocumenting/znai"
   url "https://repo.maven.apache.org/maven2/org/testingisdocumenting/znai/znai-dist/1.76/znai-dist-1.76-znai.zip"
-  sha256 "9204855a70d9b033106a15960f9fe9b0d6c9a4ddd46a9c7f4a3ec53d"
+  sha256 "f85b8010e102fc927c5448743b6215c495f125904791d3ea35168c557fd1808c"
   version "1.76"
 
   depends_on "java"
